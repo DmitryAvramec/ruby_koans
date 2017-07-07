82c7aa2e1013d3145ecad90bbdeca3858aa727ae
DmitryAvramec <dmitry.avramec@gmail.com>
2017-07-07 17:23:29 +0300

    Add files via upload

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  sum =0
+  return sum if dice ==[]
+  (1..6).each do |i|
+    counter = dice.select { |n| n == i }
+    sum += (i == 1 ? 1000 : i * 100) if counter.size >= 3
+    sum += (counter.size % 3) * 100 if i == 1
+    sum += (counter.size % 3) * 50 if i == 5
+  end
+  sum
 end
 
 class AboutScoringProject < Neo::Koan
--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,16 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  s = (a + b + c) / 2.0
+  t = (s - a) * (s - b) * (s - c)  
+  if (a * b * c) <= 0 || t <= 0 then 
+    raise TriangleError
+end
+  case [a, b, c].uniq.size
+  when 1 then :equel_sides 
+  when 2 then :equel_2_sides 
+  when 3 then :not_equel_sides
+  end 
 end
 
 # Error class used in part 2.  No need to change this code.
